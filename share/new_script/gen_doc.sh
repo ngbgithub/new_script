e560b0b6b5b3244f69cae6082a6d29bffd178b5e
ngb <anonymous@example.com>
2016-03-30 10:21:45 -0400

    Modified comment walking through document generation manual portions in gen_doc.sh.

--- share/new_script/gen_doc.sh
+++ share/new_script/gen_doc.sh
@@ -25,16 +25,34 @@ sphinx-apidoc \
 
 # Note: The following steps will have to be done manually:
 
+#   - If any executable scripts do not have the .py file extension,
+#     you may want to temporarily make a temporary copy with the same
+#     name but ending in .py before running sphinx-apidoc (above) so
+#     that sphinx-apidoc generates rst files for the scripts.
+#
+#   - Any executable script rst files may have to be renamed to
+#     reflect their installed names, and the first line of the .rst
+#     file will have to be changed to something like "foo script".  In
+#     addition, the content of the .rst script should probably reflect
+#     the output of calling the script with the --help flag, rather
+#     than having function documentation.  (The autodoc process will
+#     not update any scripts whose filename ends in .py, but since we
+#     care more about the command line for scripts, this is OK.)
+#
 #   - The copyright line in conf.py may have to be fixed to not be
 #     equal to the authors
-#
-#   - Any executable script rst files will have to be renamed to
-#     reflect their installed names, and the first line of the rst
-#     file will have to be changed to something like "foo script"
 #
 #   - Intersphinx has to be enabled by adding 'spinx.ext.intersphinx'
 #     to the extensions list.  (Note that we add some intersphinx
 #     mapping below.)
+#
+#   - You may want to explicitly list members to document in
+#       __init__.py.  Example:
+# 
+#     .. automodule:: new_script
+#         :members: foo, bar, baz
+#         :undoc-members:
+#         :show-inheritance:
 
 
 # Update sys.path in conf.py appropriately.  The following can in
@@ -53,7 +71,10 @@ sphinx-apidoc \
 #sed -i '' "s%XXREPLACE_MEXXX%sys.path.insert(0, os.path.abspath('$module_path'))%" $conf_py
 
 echo "" >> $conf_py
-echo "sys.path.insert(0, os.path.abspath('$module_path'))" >> $conf_py
+echo "d = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'bin')" \
+     >> $conf_py
+echo "sys.path.insert(0, os.path.abspath(d))" >> $conf_py
+#echo "sys.path.insert(0, os.path.abspath('$module_path'))" >> $conf_py
 
 # Add some intersphinx configuration.
 echo "intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}" >> $conf_py
